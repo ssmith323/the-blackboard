5ead4aafa807c7fc3f866633a740d5f6f85110b6
Smith <s.j.smith@accenture.com>
2021-06-03 16:38:01 -0400

    fixing login tests

--- projects/blackboard/src/app/login/login.component.spec.ts
+++ projects/blackboard/src/app/login/login.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormFieldTestingModule } from 'projects/form-fields/src/public-api';
 
+import { AuthService } from '../services/auth.service';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const auth = jasmine.createSpyObj(['signIn']);
+  auth.signIn.and.returnValue(Promise.resolve());
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [FormFieldTestingModule, RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: auth }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
