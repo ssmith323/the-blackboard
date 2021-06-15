47b213b21006d93508de6a3a232b47ff540591b4
Smith <s.j.smith@accenture.com>
2021-06-15 09:21:56 -0400

    making the abstract public

--- projects/form-fields/src/public-api.ts
+++ projects/form-fields/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export { FormFieldTestingModule } from './lib/testing/form-field-testing.module';
+export { AbstractFormHandler } from './lib/abstract-form-handler';
 export * from './lib/datepicker/datepicker.component';
 export * from './lib/input/input.component';
 export * from './lib/select/select.component';
