abafa05793a3d11331766753c3464b4e8182c97f
Smith <s.j.smith@accenture.com>
2021-06-17 10:19:48 -0400

    getting prettier on precommit

--- projects/blackboard/src/app/logged-in/create-talkingpoint/create-talkingpoint.component.ts
+++ projects/blackboard/src/app/logged-in/create-talkingpoint/create-talkingpoint.component.ts
@@ -3,9 +3,13 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { AbstractFormHandler } from 'projects/form-fields/src/lib/abstract-form-handler';
 import { BasicOption } from 'projects/form-fields/src/public-api';
+import { Observable } from 'rxjs';
 
 import { AuthService } from '../../services/auth.service';
-import { TalkingPoint, TalkingpointService } from '../services/talkingpoint.service';
+import {
+  TalkingPoint,
+  TalkingpointService,
+} from '../services/talkingpoint.service';
 
 @Component({
   selector: 'app-create-talkingpoint',
@@ -19,6 +23,7 @@ export class CreateTalkingpointComponent
   types: BasicOption<any>[];
   params: Params;
   editItem: boolean;
+  talkingPoint$!: Observable<TalkingPoint>;
 
   constructor(
     private fb: FormBuilder,
@@ -53,17 +58,17 @@ export class CreateTalkingpointComponent
       this.talkingpointService
         .getByKey(this.params.id, this.params.key)
         .subscribe((tp) => {
-          this.form.get('title')?.setValue(tp?.title);
-          this.form.get('type')?.setValue(tp?.type);
-          this.form.get('description')?.setValue(tp?.description);
+          this.getControl('title')?.setValue(tp?.title);
+          this.getControl('type')?.setValue(tp?.type);
+          this.getControl('description')?.setValue(tp?.description);
           this.form
             .get('removalDate')
             ?.setValue(new Date(tp?.removalDate || 0));
         });
     }
     this.auth.getUser().then((user) => {
-      this.form.get('name')?.setValue(user.displayName);
-      this.form.get('userId')?.setValue(user.uid);
+      this.getControl('name')?.setValue(user.displayName);
+      this.getControl('userId')?.setValue(user.uid);
     });
   }
 
