433534e26915c734d8bfd93faae35848ef805089
Stephen J Smith <ssmith323@gmail.com>
2022-07-20 20:23:38 -0400

    adding any email

--- projects/blackboard/src/app/register/validators.ts
+++ projects/blackboard/src/app/register/validators.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function emailCheck(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const forbidden = /.*@accenture.com$/.test(control.value);
+    const forbidden = /.*@.*\.[a-z]{3}$/.test(control.value);
     return !forbidden ? { emailCheck: true } : null;
   };
 }
